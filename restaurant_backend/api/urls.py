64dce073e08d6d45bcb7f5eff30a15a18ae9bbf1
TAkshayKumar09 <akshaykumart0609@gmail.com>
2025-12-06 22:42:54 +0530

    get_menu

--- restaurant_backend/api/urls.py
+++ restaurant_backend/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns =[
     path('update_user/<str:email>/', view=views.update_user),
     path('delete_user/', view=views.delete_user),
 
-    path('getMenu', view=views.get_menu),
+    path('get_menu/', view=views.get_menu),
     path('admin_only/', view=views.createmenu),
     path('update_menu/<str:name>/', view=views.update_menu),
     path('delete_item/', view=views.delete_item)
